e9723f0d98b263fb0558fd26bebba2994a4dadc6
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-05-18 23:08:09 +0200

    Check for conflict markers and whitespace errors with Git

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM alpine:3.9.4
 
+ARG git=''
 ARG prettier=''
 
 WORKDIR /workdir
+
+RUN if [[ -n "${git}" ]]; then \
+    apk add --no-cache "git==${git}" \
+    ; fi
 
 RUN if [[ -n "${prettier}" ]]; then \
     apk add --no-cache yarn \
--- ci/build.sh
+++ ci/build.sh
@@ -8,8 +8,15 @@ readonly PROJECT_FOLDER="$(dirname "${SCRIPT_FOLDER}")"
 pushd "${PROJECT_FOLDER}"
 
 docker build --tag code_checkers \
+  --build-arg git=2.20.1-r0 \
   --build-arg prettier=1.17.0 \
   .
+
+readonly COMMITS_TO_CHECK=origin/master..HEAD
+
+docker run --volume "$(pwd)":/workdir code_checkers sh -c \
+  "git rev-list --reverse ${COMMITS_TO_CHECK} \
+  | xargs -n 1 -I {} git diff --check {}^ {}"
 
 docker run --volume "$(pwd)":/workdir code_checkers \
   prettier --check '**/*.+(json|md|yaml|yml)'
