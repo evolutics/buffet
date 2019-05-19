538bc283affa911d6554500a4d941598ec21d320
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-05-19 14:32:13 +0200

    Check format of Git messages with Gitlint

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
 FROM alpine:3.9.4
 
 ARG git=''
+ARG gitlint=''
 ARG prettier=''
 
 WORKDIR /workdir
 
 RUN if [[ -n "${git}" ]]; then \
     apk add --no-cache "git==${git}" \
+    ; fi
+
+RUN if [[ -n "${gitlint}" ]]; then \
+    apk add --no-cache git python3 \
+    && pip3 install "gitlint==${gitlint}" \
     ; fi
 
 RUN if [[ -n "${prettier}" ]]; then \
--- ci/build.sh
+++ ci/build.sh
@@ -9,6 +9,7 @@ pushd "${PROJECT_FOLDER}"
 
 docker build --tag code_checkers \
   --build-arg git=2.20.1-r0 \
+  --build-arg gitlint=0.11.0 \
   --build-arg prettier=1.17.0 \
   .
 
@@ -17,6 +18,9 @@ readonly COMMITS_TO_CHECK=origin/master..HEAD
 docker run --volume "$(pwd)":/workdir code_checkers sh -c \
   "git rev-list --reverse ${COMMITS_TO_CHECK} \
   | xargs -n 1 -I {} git diff --check {}^ {}"
+
+docker run --volume "$(pwd)":/workdir code_checkers \
+  gitlint --config ci/.gitlint --commits "${COMMITS_TO_CHECK}"
 
 docker run --volume "$(pwd)":/workdir code_checkers \
   prettier --check '**/*.+(json|md|yaml|yml)'
