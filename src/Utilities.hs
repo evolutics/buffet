2ea286cdd2daecde1c657ff08bb41ad2f60f62aa
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-07-04 21:31:55 +0200

    Model command-line utilities for Docker

---
+++ src/Utilities.hs
@@ -0,0 +1,47 @@
+module Utilities
+  ( Box(..)
+  , Command(..)
+  , Documentation(..)
+  , Tag(..)
+  , Utility(..)
+  ) where
+
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Text as T
+import Prelude (Eq, Ord, Show)
+
+newtype Box =
+  Box
+    { optionToUtility :: Map.Map T.Text Utility
+    }
+  deriving (Eq, Ord, Show)
+
+data Utility =
+  Utility
+    { installation :: Command
+    , documentation :: Documentation
+    }
+  deriving (Eq, Ord, Show)
+
+newtype Command =
+  Command
+    { indentableLines :: [T.Text]
+    }
+  deriving (Eq, Ord, Show)
+
+data Documentation =
+  Documentation
+    { displayName :: T.Text
+    , link :: T.Text
+    , tags :: Set.Set Tag
+    , help :: Command
+    }
+  deriving (Eq, Ord, Show)
+
+data Tag =
+  Tag
+    { key :: T.Text
+    , value :: T.Text
+    }
+  deriving (Eq, Ord, Show)
