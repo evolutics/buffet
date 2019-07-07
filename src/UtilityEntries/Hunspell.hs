59431f2dadc3611f1add0e8b9c8668b2fca2ab83
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-07-07 15:49:15 +0200

    Provide Hunspell entry without tags yet

---
+++ src/UtilityEntries/Hunspell.hs
@@ -0,0 +1,35 @@
+module UtilityEntries.Hunspell
+  ( get
+  ) where
+
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Text as T
+import Prelude ()
+import qualified Utilities
+
+get :: Utilities.Entry
+get =
+  Utilities.Entry
+    { Utilities.option = T.pack "hunspell"
+    , Utilities.utility =
+        Utilities.Utility
+          { Utilities.installation =
+              Utilities.Command
+                { Utilities.indentableLines =
+                    [ T.pack
+                        "apk add --no-cache \"hunspell==${hunspell}\" hunspell-en \\"
+                    ]
+                }
+          , Utilities.extraOptionsWithDefaults = Map.empty
+          , Utilities.documentation =
+              Utilities.Documentation
+                { Utilities.displayName = T.pack "Hunspell"
+                , Utilities.link = T.pack "https://hunspell.github.io"
+                , Utilities.tags = Set.empty
+                , Utilities.help =
+                    Utilities.Command
+                      {Utilities.indentableLines = [T.pack "hunspell --help"]}
+                }
+          }
+    }
