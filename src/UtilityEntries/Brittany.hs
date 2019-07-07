70ef9000ba042b826618f2ec5722281b8de2b54c
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-07-07 15:36:27 +0200

    Provide brittany entry without tags yet

---
+++ src/UtilityEntries/Brittany.hs
@@ -0,0 +1,45 @@
+module UtilityEntries.Brittany
+  ( get
+  ) where
+
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Text as T
+import Prelude ()
+import qualified Utilities
+
+get :: Utilities.Entry
+get =
+  Utilities.Entry
+    { Utilities.option = T.pack "brittany"
+    , Utilities.utility =
+        Utilities.Utility
+          { Utilities.installation =
+              Utilities.Command
+                { Utilities.indentableLines =
+                    [ T.pack
+                        "apk add --no-cache cabal ghc gmp libffi musl-dev ncurses-dev wget \\"
+                    , T.pack "&& cabal update \\"
+                    , T.pack "\\"
+                    , T.pack
+                        "&& cabal install --jobs \"brittany-${brittany}\" \\"
+                    , T.pack
+                        "&& mv \"${HOME}/.cabal/bin/brittany\" /usr/local/bin/brittany \\"
+                    , T.pack "&& rm -r \"${HOME}/.cabal\" \\"
+                    , T.pack "\\"
+                    , T.pack "&& apk del cabal ghc \\"
+                    ]
+                }
+          , Utilities.extraOptionsWithDefaults = Map.empty
+          , Utilities.documentation =
+              Utilities.Documentation
+                { Utilities.displayName = T.pack "brittany"
+                , Utilities.link =
+                    T.pack "https://github.com/lspitzner/brittany"
+                , Utilities.tags = Set.empty
+                , Utilities.help =
+                    Utilities.Command
+                      {Utilities.indentableLines = [T.pack "brittany --help"]}
+                }
+          }
+    }
