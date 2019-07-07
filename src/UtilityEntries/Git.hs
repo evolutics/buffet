ae46a708fdd837f41a3c571bdfcbbf4abb466374
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-07-07 15:40:47 +0200

    Provide Git entry without tags yet

---
+++ src/UtilityEntries/Git.hs
@@ -0,0 +1,33 @@
+module UtilityEntries.Git
+  ( get
+  ) where
+
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Text as T
+import Prelude ()
+import qualified Utilities
+
+get :: Utilities.Entry
+get =
+  Utilities.Entry
+    { Utilities.option = T.pack "git"
+    , Utilities.utility =
+        Utilities.Utility
+          { Utilities.installation =
+              Utilities.Command
+                { Utilities.indentableLines =
+                    [T.pack "apk add --no-cache \"git==${git}\" \\"]
+                }
+          , Utilities.extraOptionsWithDefaults = Map.empty
+          , Utilities.documentation =
+              Utilities.Documentation
+                { Utilities.displayName = T.pack "Git"
+                , Utilities.link = T.pack "https://git-scm.com"
+                , Utilities.tags = Set.empty
+                , Utilities.help =
+                    Utilities.Command
+                      {Utilities.indentableLines = [T.pack "git --help"]}
+                }
+          }
+    }
