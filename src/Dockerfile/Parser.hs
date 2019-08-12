1936aeead65ac3ae322c376ee345d1e74b2b7f57
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-08-12 21:24:23 +0200

    Use main module of Docker library if possible

--- src/Dockerfile/Parser.hs
+++ src/Dockerfile/Parser.hs
@@ -5,7 +5,7 @@ module Dockerfile.Parser
 import qualified Data.Maybe as Maybe
 import qualified Data.Text as T
 import qualified Dockerfile.Intermediate as Intermediate
-import qualified Language.Docker.Parser as Parser
+import qualified Language.Docker as Docker
 import qualified Language.Docker.Syntax as Syntax
 import Prelude (Maybe(Just, Nothing), ($), (.), either, error, fmap, id)
 import qualified Text.Show as Show
@@ -30,17 +30,17 @@ parseUtility utility =
     rawDockerfile = Utilities.dockerfile utility
     extraOptionsWithDefaults = Utilities.extraOptionsWithDefaults utility
 
-command :: Syntax.InstructionPos a -> Maybe a
-command (Syntax.InstructionPos (Syntax.Run (Syntax.ArgumentsText argument)) _ _) =
+command :: Docker.InstructionPos a -> Maybe a
+command (Docker.InstructionPos (Docker.Run (Syntax.ArgumentsText argument)) _ _) =
   Just argument
 command _ = Nothing
 
-patchDockerfile :: Syntax.Dockerfile -> Syntax.Dockerfile
+patchDockerfile :: Docker.Dockerfile -> Docker.Dockerfile
 patchDockerfile = fmap $ fmap reviveLineBreaks
   where
     reviveLineBreaks = reviveSimpleLineBreak . reviveBlankLine
     reviveSimpleLineBreak = T.replace (T.pack "   ") $ T.pack " \\\n  "
     reviveBlankLine = T.replace (T.pack "     && ") $ T.pack " \\\n  \\\n  && "
 
-parseDockerfile :: T.Text -> Syntax.Dockerfile
-parseDockerfile = either (error . Show.show) id . Parser.parseText
+parseDockerfile :: T.Text -> Docker.Dockerfile
+parseDockerfile = either (error . Show.show) id . Docker.parseText
