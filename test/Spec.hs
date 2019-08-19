3bee51507641d8e6b13bdb704e6ae692e3d7cdd5
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-08-19 21:33:44 +0200

    Allow to test Dockerfile generation with file pairs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,22 +1,43 @@
+import qualified Control.Applicative as Applicative
+import qualified Data.List as List
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LazyT
 import qualified Data.Text.Lazy.Encoding as Encoding
 import qualified Lib
-import Prelude (FilePath, IO, ($), (.), fmap)
+import Prelude (FilePath, IO, ($), (.), (>>=), fmap, return)
+import qualified System.Directory as Directory
+import qualified System.FilePath
 import qualified Test.Tasty as Tasty
 import qualified Test.Tasty.Golden as Golden
 
 main :: IO ()
-main = Tasty.defaultMain tests
+main = tests >>= Tasty.defaultMain
 
-tests :: Tasty.TestTree
-tests =
-  Tasty.testGroup
-    "Tests"
-    [ assertFileEqualsText "Example" "test/data/dockerfile/example.out" example
-    , assertFileEqualsText "Main" "Dockerfile" Lib.dockerfile
-    ]
+tests :: IO Tasty.TestTree
+tests = do
+  generation <-
+    Tasty.testGroup "Generation" Applicative.<$>
+    generationTests "test/data/dockerfile"
+  return $
+    Tasty.testGroup
+      "Tests"
+      [generation, assertFileEqualsText "Main" "Dockerfile" Lib.dockerfile]
+
+generationTests :: FilePath -> IO [Tasty.TestTree]
+generationTests folder = do
+  files <- Directory.listDirectory folder
+  let baseNames = List.sort . List.nub $ fmap System.FilePath.takeBaseName files
+  return $ fmap assert baseNames
+  where
+    assert baseName =
+      assertFileEqualsText baseName (expected baseName) $ actual baseName
+    expected = filename "out"
+    filename extension baseName =
+      System.FilePath.combine folder $
+      System.FilePath.addExtension baseName extension
+    actual =
+      Lib.generateDockerfile . Map.singleton (T.pack "example") . filename "in"
 
 assertFileEqualsText ::
      Tasty.TestName -> FilePath -> IO T.Text -> Tasty.TestTree
@@ -27,8 +48,3 @@ assertFileEqualsText name expected actualAction =
       ["diff", "--unified", expectedFile, actualFile]
     actualBinaryAction = fmap textToBinary actualAction
     textToBinary = Encoding.encodeUtf8 . LazyT.fromStrict
-
-example :: IO T.Text
-example =
-  Lib.generateDockerfile $
-  Map.singleton (T.pack "example") "test/data/dockerfile/example.in"
