05103267d72a10faaa7e6535d11a3372bbe1c328
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-01 18:21:20 +0200

    Introduce command for current workflow

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,19 @@ module Main
   ( main
   ) where
 
+import qualified Control.Monad as Monad
 import qualified Data.Text.IO as T.IO
 import qualified Lib
-import Prelude (IO, (>>=))
+import qualified Options.Applicative as Options
+import Prelude (IO, ($), (>>=), mempty, pure)
 
 main :: IO ()
-main = Lib.build "dockerfiles" >>= T.IO.putStrLn
+main = Monad.join $ Options.execParser (Options.info commands mempty)
+
+commands :: Options.Parser (IO ())
+commands =
+  Options.hsubparser $
+  Options.command "build" (Options.info (pure build) mempty)
+
+build :: IO ()
+build = Lib.build "dockerfiles" >>= T.IO.putStrLn
