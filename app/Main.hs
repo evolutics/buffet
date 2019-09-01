5b035c9a4dd2941c981b336af2705c80b5f82a03
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-01 18:41:04 +0200

    Provide stub command for testing

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import qualified Control.Monad as Monad
 import qualified Data.Text.IO as T.IO
 import qualified Lib
 import qualified Options.Applicative as Options
-import Prelude (FilePath, IO, ($), (<$>), mempty)
+import Prelude (FilePath, IO, ($), (<$>), mconcat, mempty)
 
 main :: IO ()
 main = Monad.join $ Options.execParser (Options.info commands mempty)
@@ -14,9 +14,17 @@ main = Monad.join $ Options.execParser (Options.info commands mempty)
 commands :: Options.Parser (IO ())
 commands =
   Options.hsubparser $
-  Options.command
-    "build"
-    (Options.info (build <$> Options.argument Options.str mempty) mempty)
+  mconcat
+    [ Options.command
+        "build"
+        (Options.info (build <$> Options.argument Options.str mempty) mempty)
+    , Options.command
+        "test"
+        (Options.info (test <$> Options.argument Options.str mempty) mempty)
+    ]
 
 build :: FilePath -> IO ()
 build = Lib.build Monad.>=> T.IO.putStrLn
+
+test :: FilePath -> IO ()
+test = Lib.test
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,18 @@
 module Lib
   ( build
+  , test
   ) where
 
 import qualified Data.Text as T
 import qualified Dockerfile.Parser as Parser
 import qualified Dockerfile.Printer as Printer
-import Prelude (FilePath, IO, (.), either, error, fmap)
+import Prelude (FilePath, IO, (.), either, error, fmap, undefined)
 
 build :: FilePath -> IO T.Text
 build = fmap (either errors Printer.get) . Parser.get
   where
     errors :: [T.Text] -> a
     errors = error . T.unpack . T.unlines
+
+test :: FilePath -> IO ()
+test = undefined
