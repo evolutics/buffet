bac8ead0e1a639e56763892d3387bcd68bbf2a98
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-01 17:01:27 +0200

    Make one function only for simplicity

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,16 +3,12 @@ module Lib
   ) where
 
 import qualified Data.Text as T
-import qualified Dockerfile.Intermediate as Intermediate
 import qualified Dockerfile.Parser as Parser
 import qualified Dockerfile.Printer as Printer
-import Prelude (Either, FilePath, IO, ($), (.), either, error, fmap)
+import Prelude (FilePath, IO, (.), either, error, fmap)
 
 dockerfile :: FilePath -> IO T.Text
-dockerfile = generate . Parser.get
-
-generate :: IO (Either [T.Text] Intermediate.Box) -> IO T.Text
-generate = fmap $ either errors Printer.get
+dockerfile = fmap (either errors Printer.get) . Parser.get
   where
     errors :: [T.Text] -> a
     errors = error . T.unpack . T.unlines
