5cc6b0267b53454ef6e85bf84d7509ef3ee65806
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-09 20:57:17 +0200

    Remove unused Docker predicate

--- src/Buffet/Toolbox/DockerTools.hs
+++ src/Buffet/Toolbox/DockerTools.hs
@@ -1,14 +1,9 @@
 module Buffet.Toolbox.DockerTools
-  ( isArg
-  , isFrom
+  ( isFrom
   ) where
 
 import qualified Language.Docker as Docker
 import Prelude (Bool(False, True))
-
-isArg :: Docker.Instruction a -> Bool
-isArg (Docker.Arg _ _) = True
-isArg _ = False
 
 isFrom :: Docker.Instruction a -> Bool
 isFrom (Docker.From _) = True
