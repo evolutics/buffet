37274494f111b76d1ab5ad92ce0f317374654a2d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-09 18:37:25 +0200

    Merge parse tools into only module that uses them

--- src/Buffet/Parse/ParseInternal.hs
+++ src/Buffet/Parse/ParseInternal.hs
@@ -6,17 +6,20 @@ import qualified Buffet.Ir.Ir as Ir
 import qualified Buffet.Parse.GetSourcePaths as GetSourcePaths
 import qualified Buffet.Parse.ParseInstructionPartition as ParseInstructionPartition
 import qualified Buffet.Parse.ParseTestCommand as ParseTestCommand
-import qualified Buffet.Parse.ParseTools as ParseTools
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 import qualified Language.Docker as Docker
-import Prelude (FilePath, IO, ($), fmap, mapM, pure)
+import Prelude (FilePath, IO, ($), (.), either, error, fmap, id, mapM, pure)
+import qualified Text.Show as Show
 
 get :: FilePath -> IO Ir.Buffet
 get buffetPath = do
   optionToDishPath <- GetSourcePaths.get buffetPath
-  optionToDish <- mapM ParseTools.parseDockerfile optionToDishPath
+  optionToDish <- mapM parseDockerfile optionToDishPath
   pure $ parseBuffet optionToDish
+
+parseDockerfile :: FilePath -> IO Docker.Dockerfile
+parseDockerfile = fmap (either (error . Show.show) id) . Docker.parseFile
 
 parseBuffet :: Map.Map T.Text Docker.Dockerfile -> Ir.Buffet
 parseBuffet optionToDish =
@@ -29,4 +32,11 @@ parseDish rawDockerfile =
     , Ir.testCommand = ParseTestCommand.get dockerfile
     }
   where
-    dockerfile = ParseTools.patchDockerfile rawDockerfile
+    dockerfile = patchDockerfile rawDockerfile
+
+patchDockerfile :: Docker.Dockerfile -> Docker.Dockerfile
+patchDockerfile = fmap $ fmap reviveLineBreaks
+  where
+    reviveLineBreaks = reviveSimpleLineBreak . reviveBlankLine
+    reviveSimpleLineBreak = T.replace (T.pack "   ") $ T.pack " \\\n  "
+    reviveBlankLine = T.replace (T.pack "     && ") $ T.pack " \\\n  \\\n  && "
