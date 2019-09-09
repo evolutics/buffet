fda2dd98ef201f5e8f276e96c92d7afc4f76f32e
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-09 21:52:12 +0200

    Move remaining Docker tool to only user

--- src/Buffet/Build/PrepareOptionArgInstruction.hs
+++ src/Buffet/Build/PrepareOptionArgInstruction.hs
@@ -3,16 +3,19 @@ module Buffet.Build.PrepareOptionArgInstruction
   ) where
 
 import qualified Buffet.Ir.Ir as Ir
-import qualified Buffet.Toolbox.DockerTools as DockerTools
 import qualified Data.List as List
 import qualified Data.Text as T
 import qualified Language.Docker as Docker hiding (sourcePaths)
-import Prelude (Maybe(Nothing), (<>), span)
+import Prelude (Bool(False, True), Maybe(Nothing), (<>), span)
 
 get :: T.Text -> Ir.DockerfilePart -> Ir.DockerfilePart
 get option stage = firstFroms <> preparedAfterFirstFroms
   where
-    (firstFroms, afterFirstFroms) = span DockerTools.isFrom stage
+    (firstFroms, afterFirstFroms) = span isFrom stage
     preparedAfterFirstFroms = optionArg : List.delete optionArg afterFirstFroms
     optionArg :: Docker.Instruction T.Text
     optionArg = Docker.Arg option Nothing
+
+isFrom :: Docker.Instruction a -> Bool
+isFrom (Docker.From _) = True
+isFrom _ = False
