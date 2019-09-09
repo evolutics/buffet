68b4b338876191ad5291efae71bcda435da53a75
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-09 16:32:10 +0200

    Print full buffet as JSON

--- src/Buffet/Parse/Print.hs
+++ src/Buffet/Parse/Print.hs
@@ -8,18 +8,35 @@ module Buffet.Parse.Print
 import qualified Buffet.Ir.Ir as Ir
 import qualified Buffet.Toolbox.TextTools as TextTools
 import qualified Data.Aeson as Aeson
+import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as Lazy
 import qualified GHC.Generics as Generics
-import Prelude (Eq, Ord, Show, (.))
+import qualified Language.Docker as Docker
+import Prelude (Eq, Maybe, Ord, Show, ($), (.), (<$>), fmap)
 
-data Buffet =
+newtype Buffet =
   Buffet
-    {
+    { optionToDish :: Map.Map T.Text Dish
     }
   deriving (Eq, Generics.Generic, Ord, Show)
 
 instance Aeson.ToJSON Buffet where
   toEncoding = Aeson.genericToEncoding options
+
+data Dish =
+  Dish
+    { beforeFirstBuildStage :: DockerfilePart
+    , localBuildStages :: [DockerfilePart]
+    , globalBuildStage :: DockerfilePart
+    , testCommand :: Maybe T.Text
+    }
+  deriving (Eq, Generics.Generic, Ord, Show)
+
+instance Aeson.ToJSON Dish where
+  toEncoding = Aeson.genericToEncoding options
+
+type DockerfilePart = [T.Text]
 
 options :: Aeson.Options
 options = Aeson.defaultOptions {Aeson.fieldLabelModifier = Aeson.camelTo2 '_'}
@@ -28,4 +45,23 @@ get :: Ir.Buffet -> T.Text
 get = TextTools.decodeUtf8 . Aeson.encode . transformBuffet
 
 transformBuffet :: Ir.Buffet -> Buffet
-transformBuffet _ = Buffet {}
+transformBuffet buffet =
+  Buffet {optionToDish = transformDish <$> Ir.optionToDish buffet}
+
+transformDish :: Ir.Dish -> Dish
+transformDish dish =
+  Dish
+    { beforeFirstBuildStage =
+        transformDockerfilePart $ Ir.beforeFirstBuildStage dish
+    , localBuildStages = transformDockerfilePart <$> Ir.localBuildStages dish
+    , globalBuildStage = transformDockerfilePart $ Ir.globalBuildStage dish
+    , testCommand = Ir.testCommand dish
+    }
+
+transformDockerfilePart :: Ir.DockerfilePart -> DockerfilePart
+transformDockerfilePart = fmap transformInstruction
+
+transformInstruction :: Docker.Instruction T.Text -> T.Text
+transformInstruction instruction =
+  T.stripEnd . Lazy.toStrict $
+  Docker.prettyPrint [Docker.instructionPos instruction]
--- test/data/build/drops_command_healthcheck/expected.Dockerfile
+++
@@ -1,8 +0,0 @@
-ARG alpine_version='3.9.4'
-ARG example=''
-
-FROM alpine:"${alpine_version}"
-
-ARG example
-
-WORKDIR /workdir
