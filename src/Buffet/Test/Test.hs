829dacddcbe73b141e6630cbe8d70897384de376
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-16 22:22:59 +0200

    Separate testing command logic from parsing

--- src/Buffet/Test/Test.hs
+++ src/Buffet/Test/Test.hs
@@ -2,75 +2,13 @@ module Buffet.Test.Test
   ( get
   ) where
 
-import qualified Buffet.Build.BuildInternal as BuildInternal
-import qualified Buffet.Ir.Ir as Ir
 import qualified Buffet.Parse.ParseInternal as ParseInternal
-import qualified Buffet.Toolbox.TextTools as TextTools
-import qualified Data.Map.Strict as Map
-import qualified Data.Text as T
-import qualified Data.Text.IO as T.IO
+import qualified Buffet.Test.TestInternal as TestInternal
 import qualified Data.Yaml as Yaml
-import Prelude
-  ( FilePath
-  , IO
-  , Maybe(Just, Nothing)
-  , ($)
-  , (.)
-  , concatMap
-  , mconcat
-  , not
-  , pure
-  , sequence_
-  )
-import qualified System.IO as IO
-import qualified System.Process.Typed as Process
+import Prelude (FilePath, IO)
 
 get :: FilePath -> FilePath -> IO ()
 get buffetSource argumentsFile = do
-  buffetIr <- ParseInternal.get buffetSource
-  let buffet = BuildInternal.get buffetIr
+  buffet <- ParseInternal.get buffetSource
   arguments <- Yaml.decodeFileThrow argumentsFile
-  let _ = arguments :: Map.Map T.Text T.Text
-  imageId <- dockerBuild buffet arguments
-  let optionToDish = filterTestedDishes (Ir.optionToDish buffetIr) arguments
-      tests =
-        Map.mapWithKey
-          (\option dish ->
-             case Ir.testCommand dish of
-               Nothing ->
-                 putStderrLine $ mconcat [T.pack "No test for dish: ", option]
-               Just testCommand ->
-                 Process.runProcess_ $
-                 Process.proc
-                   "docker"
-                   ["run", T.unpack imageId, "sh", "-c", T.unpack testCommand])
-          optionToDish
-  sequence_ tests
-
-dockerBuild :: T.Text -> Map.Map T.Text T.Text -> IO T.Text
-dockerBuild dockerfile arguments = do
-  rawImageIdLine <-
-    Process.readProcessStdout_ $
-    Process.setStdin (textInput dockerfile) processBase
-  let imageIdLine = TextTools.decodeUtf8 rawImageIdLine
-      imageId = T.stripEnd imageIdLine
-  pure imageId
-  where
-    textInput = Process.byteStringInput . TextTools.encodeUtf8
-    processBase =
-      Process.proc "docker" $ mconcat [["build", "--quiet"], buildArgs, ["-"]]
-    buildArgs =
-      concatMap
-        (\(key, value) ->
-           ["--build-arg", mconcat [T.unpack key, "=", T.unpack value]]) $
-      Map.toAscList arguments
-
-filterTestedDishes ::
-     Map.Map T.Text Ir.Dish -> Map.Map T.Text T.Text -> Map.Map T.Text Ir.Dish
-filterTestedDishes optionToDish arguments =
-  Map.restrictKeys optionToDish relevantArgumentOptions
-  where
-    relevantArgumentOptions = Map.keysSet $ Map.filter (not . T.null) arguments
-
-putStderrLine :: T.Text -> IO ()
-putStderrLine = T.IO.hPutStrLn IO.stderr
+  TestInternal.get buffet arguments
---
+++ src/Buffet/Test/TestInternal.hs
@@ -0,0 +1,70 @@
+module Buffet.Test.TestInternal
+  ( get
+  ) where
+
+import qualified Buffet.Build.BuildInternal as BuildInternal
+import qualified Buffet.Ir.Ir as Ir
+import qualified Buffet.Toolbox.TextTools as TextTools
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as T.IO
+import Prelude
+  ( IO
+  , Maybe(Just, Nothing)
+  , ($)
+  , (.)
+  , concatMap
+  , mconcat
+  , not
+  , pure
+  , sequence_
+  )
+import qualified System.IO as IO
+import qualified System.Process.Typed as Process
+
+get :: Ir.Buffet -> Map.Map T.Text T.Text -> IO ()
+get buffetIr arguments = do
+  let buffet = BuildInternal.get buffetIr
+  imageId <- dockerBuild buffet arguments
+  let optionToDish = filterTestedDishes (Ir.optionToDish buffetIr) arguments
+      tests =
+        Map.mapWithKey
+          (\option dish ->
+             case Ir.testCommand dish of
+               Nothing ->
+                 putStderrLine $ mconcat [T.pack "No test for dish: ", option]
+               Just testCommand ->
+                 Process.runProcess_ $
+                 Process.proc
+                   "docker"
+                   ["run", T.unpack imageId, "sh", "-c", T.unpack testCommand])
+          optionToDish
+  sequence_ tests
+
+dockerBuild :: T.Text -> Map.Map T.Text T.Text -> IO T.Text
+dockerBuild dockerfile arguments = do
+  rawImageIdLine <-
+    Process.readProcessStdout_ $
+    Process.setStdin (textInput dockerfile) processBase
+  let imageIdLine = TextTools.decodeUtf8 rawImageIdLine
+      imageId = T.stripEnd imageIdLine
+  pure imageId
+  where
+    textInput = Process.byteStringInput . TextTools.encodeUtf8
+    processBase =
+      Process.proc "docker" $ mconcat [["build", "--quiet"], buildArgs, ["-"]]
+    buildArgs =
+      concatMap
+        (\(key, value) ->
+           ["--build-arg", mconcat [T.unpack key, "=", T.unpack value]]) $
+      Map.toAscList arguments
+
+filterTestedDishes ::
+     Map.Map T.Text Ir.Dish -> Map.Map T.Text T.Text -> Map.Map T.Text Ir.Dish
+filterTestedDishes optionToDish arguments =
+  Map.restrictKeys optionToDish relevantArgumentOptions
+  where
+    relevantArgumentOptions = Map.keysSet $ Map.filter (not . T.null) arguments
+
+putStderrLine :: T.Text -> IO ()
+putStderrLine = T.IO.hPutStrLn IO.stderr
