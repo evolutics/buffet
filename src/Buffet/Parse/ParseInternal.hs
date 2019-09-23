6b7504900da44b2792ff5b6c228a0966e93b8d98
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-23 21:24:05 +0200

    Throw pretty exception if Dockerfile parsing fails

--- src/Buffet/Parse/ParseInternal.hs
+++ src/Buffet/Parse/ParseInternal.hs
@@ -7,21 +7,20 @@ import qualified Buffet.Parse.GetSourcePaths as GetSourcePaths
 import qualified Buffet.Parse.ParseInstructionPartition as ParseInstructionPartition
 import qualified Buffet.Parse.ParseMetadata as ParseMetadata
 import qualified Buffet.Parse.ParseTestCommand as ParseTestCommand
+import qualified Control.Exception as Exception
+import qualified Control.Monad as Monad
 import qualified Data.Map.Strict as Map
 import qualified Language.Docker as Docker
-import Prelude
-  ( FilePath
-  , IO
-  , ($)
-  , (.)
-  , either
-  , error
-  , fmap
-  , id
-  , mapM
-  , pure
-  , show
-  )
+import qualified Language.Docker.Parser as Parser
+import Prelude (FilePath, IO, Show, ($), (.), either, fmap, mapM, pure, show)
+
+newtype Exception =
+  Exception Parser.Error
+
+instance Show Exception where
+  show (Exception error) = Docker.errorBundlePretty error
+
+instance Exception.Exception Exception
 
 get :: FilePath -> IO Ir.Buffet
 get buffetPath = do
@@ -30,7 +29,8 @@ get buffetPath = do
   pure $ parseBuffet optionToDish
 
 parseDockerfile :: FilePath -> IO Docker.Dockerfile
-parseDockerfile = fmap (either (error . show) id) . Docker.parseFile
+parseDockerfile =
+  Docker.parseFile Monad.>=> either (Exception.throwIO . Exception) pure
 
 parseBuffet :: Map.Map Ir.Option Docker.Dockerfile -> Ir.Buffet
 parseBuffet optionToDish =
