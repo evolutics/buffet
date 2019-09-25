f7ebabe5ba14430f0d71be0ee6d2ecc0baff5619
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-25 20:19:13 +0200

    Throw pretty exception if arguments file parsing fails

--- src/Buffet/Test/Test.hs
+++ src/Buffet/Test/Test.hs
@@ -4,11 +4,22 @@ module Buffet.Test.Test
 
 import qualified Buffet.Parse.ParseInternal as ParseInternal
 import qualified Buffet.Test.TestInternal as TestInternal
+import qualified Control.Exception as Exception
 import qualified Data.Yaml as Yaml
-import Prelude (FilePath, IO)
+import Prelude (FilePath, IO, Show, (.), (>>=), either, pure, show)
+
+newtype Exception =
+  Exception Yaml.ParseException
+
+instance Show Exception where
+  show (Exception exception) = Yaml.prettyPrintParseException exception
+
+instance Exception.Exception Exception
 
 get :: FilePath -> FilePath -> IO ()
 get buffetSource argumentsFile = do
   buffet <- ParseInternal.get buffetSource
-  arguments <- Yaml.decodeFileThrow argumentsFile
+  arguments <-
+    Yaml.decodeFileEither argumentsFile >>=
+    either (Exception.throwIO . Exception) pure
   TestInternal.get buffet arguments
