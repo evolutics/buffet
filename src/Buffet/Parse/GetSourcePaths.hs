767fba767f60dd3817ad5a1c04579b0f215c77d9
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-25 20:10:24 +0200

    Throw pretty exception if Buffet file parsing fails

--- src/Buffet/Parse/GetSourcePaths.hs
+++ src/Buffet/Parse/GetSourcePaths.hs
@@ -3,13 +3,22 @@ module Buffet.Parse.GetSourcePaths
   ) where
 
 import qualified Buffet.Ir.Ir as Ir
+import qualified Control.Exception as Exception
 import qualified Control.Monad as Monad
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 import qualified Data.Yaml as Yaml
-import Prelude (FilePath, IO, ($), (.), fmap, pure)
+import Prelude (FilePath, IO, Show, ($), (.), (>>=), either, fmap, pure, show)
 import qualified System.Directory as Directory
 import qualified System.FilePath as FilePath
+
+newtype Exception =
+  ParseException Yaml.ParseException
+
+instance Show Exception where
+  show (ParseException exception) = Yaml.prettyPrintParseException exception
+
+instance Exception.Exception Exception
 
 get :: FilePath -> IO (Map.Map Ir.Option FilePath)
 get buffetPath = do
@@ -36,7 +45,9 @@ getFromFolder buffetFolder = do
 
 getFromFile :: FilePath -> IO (Map.Map Ir.Option FilePath)
 getFromFile buffetFile = do
-  unresolvedOptionToDish <- Yaml.decodeFileThrow buffetFile
+  unresolvedOptionToDish <-
+    Yaml.decodeFileEither buffetFile >>=
+    either (Exception.throwIO . ParseException) pure
   let optionToDish = fmap (FilePath.combine folder) unresolvedOptionToDish
   pure optionToDish
   where
