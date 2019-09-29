ac5b285a6cd2be1ec3f42eedb6f520d64dc304f9
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-09-29 12:18:26 +0200

    Log stdout and stderr when using Docker image

--- src/Buffet/Test/TestInternal.hs
+++ src/Buffet/Test/TestInternal.hs
@@ -17,7 +17,7 @@ import qualified System.IO as IO
 type TestResults = Map.Map Ir.Option TestDish.TestResult
 
 get :: Ir.Buffet -> Map.Map Ir.Option T.Text -> IO (Bool, T.Text)
-get buffet arguments = UsingDockerImage.get use dockerBuild
+get buffet arguments = UsingDockerImage.get use configuration
   where
     use imageId = evaluateTestResults <$> sequenceA tests
       where
@@ -25,16 +25,21 @@ get buffet arguments = UsingDockerImage.get use dockerBuild
         test option dish =
           TestDish.get
             TestSetup.TestSetup
-              { TestSetup.log = IO.stderr
+              { TestSetup.log = log
               , TestSetup.imageId = imageId
               , TestSetup.option = option
               , TestSetup.optionValue = Map.lookup option arguments
               , TestSetup.dish = dish
               }
-    dockerBuild =
-      UsingDockerImage.DockerBuild
-        { UsingDockerImage.dockerfile = BuildInternal.get buffet
-        , UsingDockerImage.arguments = arguments
+    log = IO.stderr
+    configuration =
+      UsingDockerImage.Configuration
+        { UsingDockerImage.log = log
+        , UsingDockerImage.dockerBuild =
+            UsingDockerImage.DockerBuild
+              { UsingDockerImage.dockerfile = BuildInternal.get buffet
+              , UsingDockerImage.arguments = arguments
+              }
         }
 
 evaluateTestResults :: TestResults -> (Bool, T.Text)
--- src/Buffet/Test/UsingDockerImage.hs
+++ src/Buffet/Test/UsingDockerImage.hs
@@ -1,5 +1,6 @@
 module Buffet.Test.UsingDockerImage
-  ( DockerBuild(..)
+  ( Configuration(..)
+  , DockerBuild(..)
   , get
   ) where
 
@@ -9,7 +10,15 @@ import qualified Control.Exception as Exception
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 import Prelude (Eq, IO, Ord, Show, ($), (.), concatMap, mconcat, pure)
+import qualified System.IO as IO
 import qualified System.Process.Typed as Process
+
+data Configuration =
+  Configuration
+    { log :: IO.Handle
+    , dockerBuild :: DockerBuild
+    }
+  deriving (Eq, Show)
 
 data DockerBuild =
   DockerBuild
@@ -18,19 +27,25 @@ data DockerBuild =
     }
   deriving (Eq, Ord, Show)
 
-get :: (T.Text -> IO a) -> DockerBuild -> IO a
-get useImage build = Exception.bracket (buildImage build) removeImage useImage
+get :: (T.Text -> IO a) -> Configuration -> IO a
+get useImage configuration =
+  Exception.bracket
+    (buildImage configuration)
+    (removeImage configuration)
+    useImage
 
-buildImage :: DockerBuild -> IO T.Text
-buildImage build = do
+buildImage :: Configuration -> IO T.Text
+buildImage configuration = do
   rawImageIdLine <-
-    Process.readProcessStdout_ $
-    Process.setStdin (textInput $ dockerfile build) processBase
+    Process.readProcessStdout_ . Process.setStderr (Process.useHandleOpen log') $
+    Process.setStdin (textInput dockerfile') processBase
   let imageIdLine = TextTools.decodeUtf8 rawImageIdLine
       imageId = T.stripEnd imageIdLine
   pure imageId
   where
+    log' = log configuration
     textInput = Process.byteStringInput . TextTools.encodeUtf8
+    dockerfile' = dockerfile $ dockerBuild configuration
     processBase =
       Process.proc "docker" $ mconcat [["build", "--quiet"], buildArgs, ["-"]]
     buildArgs =
@@ -38,11 +53,15 @@ buildImage build = do
         (\(key, value) ->
            [ "--build-arg"
            , mconcat [T.unpack $ Ir.option key, "=", T.unpack value]
-           ]) .
-      Map.toAscList $
-      arguments build
+           ]) $
+      Map.toAscList arguments'
+    arguments' = arguments $ dockerBuild configuration
 
-removeImage :: T.Text -> IO ()
-removeImage imageId =
-  Process.runProcess_ . Process.setStdout Process.nullStream $
+removeImage :: Configuration -> T.Text -> IO ()
+removeImage configuration imageId =
+  Process.runProcess_ .
+  Process.setStderr (Process.useHandleOpen log') .
+  Process.setStdout (Process.useHandleOpen log') $
   Process.proc "docker" ["rmi", T.unpack imageId]
+  where
+    log' = log configuration
