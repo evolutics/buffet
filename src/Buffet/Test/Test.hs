8f73f7947363f9dedc3a957596b5c1d2a24fc7e2
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-10-01 07:36:03 +0200

    Move arguments parsing to only parse Buffet at top

--- src/Buffet/Test/Test.hs
+++ src/Buffet/Test/Test.hs
@@ -4,13 +4,10 @@ module Buffet.Test.Test
 
 import qualified Buffet.Parse.ParseInternal as ParseInternal
 import qualified Buffet.Test.Configuration as Configuration
-import qualified Buffet.Test.ParseArguments as ParseArguments
 import qualified Buffet.Test.TestInternal as TestInternal
+import qualified Control.Monad as Monad
 import qualified Data.Text as T
 import Prelude (Bool, FilePath, IO)
 
 get :: Configuration.Configuration -> FilePath -> IO (Bool, T.Text)
-get configuration buffetSource = do
-  buffet <- ParseInternal.get buffetSource
-  arguments <- ParseArguments.get configuration
-  TestInternal.get buffet arguments
+get configuration = ParseInternal.get Monad.>=> TestInternal.get configuration
--- src/Buffet/Test/TestInternal.hs
+++ src/Buffet/Test/TestInternal.hs
@@ -4,6 +4,8 @@ module Buffet.Test.TestInternal
 
 import qualified Buffet.Build.BuildInternal as BuildInternal
 import qualified Buffet.Ir.Ir as Ir
+import qualified Buffet.Test.Configuration as Configuration
+import qualified Buffet.Test.ParseArguments as ParseArguments
 import qualified Buffet.Test.TestDish as TestDish
 import qualified Buffet.Test.TestResult as TestResult
 import qualified Buffet.Test.TestSetup as TestSetup
@@ -17,31 +19,33 @@ import qualified System.IO as IO
 
 type TestResults = Map.Map Ir.Option TestResult.TestResult
 
-get :: Ir.Buffet -> Map.Map Ir.Option T.Text -> IO (Bool, T.Text)
-get buffet arguments = UsingDockerImage.get use configuration
+get :: Configuration.Configuration -> Ir.Buffet -> IO (Bool, T.Text)
+get configuration buffet = do
+  arguments <- ParseArguments.get configuration
+  let use image = evaluateTestResults <$> sequenceA tests
+        where
+          tests = Map.mapWithKey test $ Ir.optionToDish buffet
+          test option dish =
+            TestDish.get
+              TestSetup.TestSetup
+                { TestSetup.log = log
+                , TestSetup.image = image
+                , TestSetup.option = option
+                , TestSetup.optionValue = Map.lookup option arguments
+                , TestSetup.dish = dish
+                }
+      imageConfiguration =
+        UsingDockerImage.Configuration
+          { UsingDockerImage.log = log
+          , UsingDockerImage.dockerBuild =
+              UsingDockerImage.DockerBuild
+                { UsingDockerImage.dockerfile = BuildInternal.get buffet
+                , UsingDockerImage.arguments = arguments
+                }
+          }
+  UsingDockerImage.get use imageConfiguration
   where
-    use image = evaluateTestResults <$> sequenceA tests
-      where
-        tests = Map.mapWithKey test $ Ir.optionToDish buffet
-        test option dish =
-          TestDish.get
-            TestSetup.TestSetup
-              { TestSetup.log = log
-              , TestSetup.image = image
-              , TestSetup.option = option
-              , TestSetup.optionValue = Map.lookup option arguments
-              , TestSetup.dish = dish
-              }
     log = IO.stderr
-    configuration =
-      UsingDockerImage.Configuration
-        { UsingDockerImage.log = log
-        , UsingDockerImage.dockerBuild =
-            UsingDockerImage.DockerBuild
-              { UsingDockerImage.dockerfile = BuildInternal.get buffet
-              , UsingDockerImage.arguments = arguments
-              }
-        }
 
 evaluateTestResults :: TestResults -> (Bool, T.Text)
 evaluateTestResults testResults =
