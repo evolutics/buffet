8feecdc434ac0f949f47699806ed3fd68ff02270
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-10-03 20:31:59 +0200

    Separate transforming menu to buffet for clarity

--- src/Buffet/Parse/ParseInternal.hs
+++ src/Buffet/Parse/ParseInternal.hs
@@ -10,10 +10,10 @@ import qualified Buffet.Parse.ParseMenu as ParseMenu
 import qualified Buffet.Parse.ParseMetadata as ParseMetadata
 import qualified Buffet.Toolbox.ExceptionTools as ExceptionTools
 import qualified Control.Exception as Exception
-import qualified Data.Map.Strict as Map
+import qualified Control.Monad as Monad
 import qualified Language.Docker as Docker
 import qualified Language.Docker.Parser as Parser
-import Prelude (FilePath, IO, Show, ($), (.), (<$>), fmap, pure, show)
+import Prelude (FilePath, IO, Show, ($), (.), fmap, pure, show)
 
 newtype Exception =
   Exception Parser.Error
@@ -24,19 +24,17 @@ instance Show Exception where
 instance Exception.Exception Exception
 
 get :: FilePath -> IO Ir.Buffet
-get menuSource = do
-  menu <- ParseMenu.get menuSource
-  let optionToDishAction = parseDockerfile <$> Menu.optionToDish menu
+get = ParseMenu.get Monad.>=> parseBuffet
+
+parseBuffet :: Menu.Menu -> IO Ir.Buffet
+parseBuffet menu = do
   optionToDish <-
-    ExceptionTools.sequenceAccumulatingExceptions optionToDishAction
-  pure $ parseBuffet optionToDish
+    ExceptionTools.sequenceAccumulatingExceptions . fmap parseDockerfile $
+    Menu.optionToDish menu
+  pure Ir.Buffet {Ir.optionToDish = fmap parseDish optionToDish}
 
 parseDockerfile :: FilePath -> IO Docker.Dockerfile
 parseDockerfile = ExceptionTools.eitherThrow Exception . Docker.parseFile
-
-parseBuffet :: Map.Map Ir.Option Docker.Dockerfile -> Ir.Buffet
-parseBuffet optionToDish =
-  Ir.Buffet {Ir.optionToDish = fmap parseDish optionToDish}
 
 parseDish :: Docker.Dockerfile -> Ir.Dish
 parseDish dockerfile =
