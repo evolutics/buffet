5be47b196f802b8c66e9707b8ea0e2ecee06f3cf
Benjamin Fischer <benjamin.fischer@evolutics.info>
2020-10-12 20:40:44 +0200

    Keep line layout of health check command for pretty printing

--- src/Buffet/Parse/ParseHealthCheck.hs
+++ src/Buffet/Parse/ParseHealthCheck.hs
@@ -2,17 +2,17 @@ module Buffet.Parse.ParseHealthCheck
   ( get
   ) where
 
+import qualified Buffet.Toolbox.DockerTools as DockerTools
 import qualified Data.Maybe as Maybe
 import qualified Data.Text as T
 import qualified Language.Docker as Docker
-import qualified Language.Docker.Syntax as Syntax
 import Prelude (Maybe(Just, Nothing), ($), (.), reverse)
 
 get :: Docker.Dockerfile -> Maybe T.Text
 get stage =
   case lastHealthcheck stage of
     Just (Docker.Check checkArguments) ->
-      Just . argumentsText $ Docker.checkCommand checkArguments
+      Just . DockerTools.printArguments $ Docker.checkCommand checkArguments
     _ -> Nothing
 
 lastHealthcheck :: Docker.Dockerfile -> Maybe (Docker.Check T.Text)
@@ -21,7 +21,3 @@ lastHealthcheck = Maybe.listToMaybe . reverse . Maybe.mapMaybe maybeHealthcheck
     maybeHealthcheck (Docker.InstructionPos (Docker.Healthcheck check) _ _) =
       Just check
     maybeHealthcheck _ = Nothing
-
-argumentsText :: Docker.Arguments T.Text -> T.Text
-argumentsText (Syntax.ArgumentsText text) = text
-argumentsText (Syntax.ArgumentsList list) = list
--- src/Buffet/Toolbox/DockerTools.hs
+++ src/Buffet/Toolbox/DockerTools.hs
@@ -1,12 +1,20 @@
 module Buffet.Toolbox.DockerTools
-  ( printInstruction
+  ( printArguments
+  , printInstruction
   ) where
 
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as Lazy
+import qualified Data.Text.Prettyprint.Doc as Doc
+import qualified Data.Text.Prettyprint.Doc.Render.Text as Text
 import qualified Language.Docker as Docker
+import qualified Language.Docker.PrettyPrint as PrettyPrint
 import qualified Language.Docker.Syntax as Syntax
-import Prelude (($), mempty)
+import Prelude (($), (.), mempty)
+
+printArguments :: Docker.Arguments T.Text -> T.Text
+printArguments =
+  Text.renderStrict . Doc.layoutCompact . PrettyPrint.prettyPrintArguments
 
 printInstruction :: Docker.Instruction T.Text -> T.Text
 printInstruction instruction =
